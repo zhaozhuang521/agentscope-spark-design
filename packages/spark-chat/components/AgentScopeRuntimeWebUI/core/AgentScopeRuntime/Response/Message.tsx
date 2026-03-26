b0eddfb52aaada4f9ad2554da8931278d8cbf06e
lishengzxc <306009337@qq.com>
2026-03-26 10:31:27 +0800

    feat(chat): add image support in message rendering
    - Enhanced the Message component to handle image content by introducing a new case for rendering images using the Images component.
    - Updated the Images component name for clarity and consistency in the codebase.

--- packages/spark-chat/components/AgentScopeRuntimeWebUI/core/AgentScopeRuntime/Response/Message.tsx
+++ packages/spark-chat/components/AgentScopeRuntimeWebUI/core/AgentScopeRuntime/Response/Message.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Markdown } from '@agentscope-ai/chat';
 import { AgentScopeRuntimeContentType, AgentScopeRuntimeRunStatus, IAgentScopeRuntimeMessage } from "../types";
+import Images from '../../../../DefaultCards/Images';
 
 const Message = React.memo(function ({ data }: { data: IAgentScopeRuntimeMessage }) {
   if (!data.content?.length) return null;
@@ -13,6 +14,8 @@ const Message = React.memo(function ({ data }: { data: IAgentScopeRuntimeMessage
             return <Markdown key={index} content={item.text} cursor={item.status === AgentScopeRuntimeRunStatus.InProgress ? true : false}></Markdown>
           case AgentScopeRuntimeContentType.REFUSAL:
             return <Markdown raw key={index} content={item.refusal}></Markdown>
+          case AgentScopeRuntimeContentType.IMAGE:
+            return <Images key={index} data={[{ url: item.image_url }]}></Images>
           default:
             return <div key={index}>{JSON.stringify(item)}</div>
         }
